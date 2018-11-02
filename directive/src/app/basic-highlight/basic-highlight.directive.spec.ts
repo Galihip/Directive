cd2d7bf11b89d1638d1de36cd0dff89aeac4dd94
Galihip <44691389+Galihip@users.noreply.github.com>
2018-11-02 11:31:56 +0700

    Add files via upload

---
+++ directive/src/app/basic-highlight/basic-highlight.directive.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { BasicHighlight} from './basic-highlight/basic-highlight.directive.ts';
+
+describe('Directive: BasicHighlight.directive.spec.ts', () => {
+  it('should create an instance', () => {
+    const directive = new BasicHighlight.directive.spec.tsDirective();
+    expect(directive).toBeTruthy();
+  });
+});
